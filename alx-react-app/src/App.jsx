6aafdd158529f7d4b6c2184b319cb75f09289eee
BWangui <brigidtwangui@outlook.com>
2025-02-11 17:17:10 +0300

    added header.jsx,footer.jsx and maincontent.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  return (
+  return(
     <div>
-      <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
     </div>
-
   );
+
 }
 
 export default App;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return(
+        <main>
+            <p>I love to visit New York, PParis, and Tokyo.</p>
+        </main>
+
+    );
+
+}
+
+export default MainContent;
