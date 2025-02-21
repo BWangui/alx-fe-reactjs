0a9e6e5f503e5ccafc68a830778cd665d7f550ec
BWangui <brigidtwangui@outlook.com>
2025-02-21 13:17:21 +0300

    edited UserContext.js

--- alx-react-app-props/src/UserContext
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const UserContext = createContext(); // Create Context
-
-export default UserContext;
--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext } from "./UserContext";
 
 const UserContext = createContext(null);
 
