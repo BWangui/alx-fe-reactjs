d7cda4c58a6f82ad189e648bf5556e09f284941c
BWangui <brigidtwangui@outlook.com>
2025-02-21 13:34:24 +0300

    edited 5.7

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react';
-import UserContext from './UserContext';
-
-function UserDetails({userData}) {
-  const userData = useContext(UserContext); 
-
-  return (
-    <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
-}
-
-export default UserDetails;
+function UserDetails({ userData }) {
+    return (
+      <div>
+        <p>Name: {userData.name}</p>
+        <p>Email: {userData.email}</p>
+      </div>
+    );
+  }
+  
+  export default UserDetails;
