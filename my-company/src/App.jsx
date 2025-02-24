fb7c4d887f87f0bd7f031ae983364f76dca8e577
BWangui <brigidtwangui@outlook.com>
2025-02-24 21:04:38 +0300

    Edit 1.3

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
