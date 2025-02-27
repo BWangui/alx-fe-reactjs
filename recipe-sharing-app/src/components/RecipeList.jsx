f3e5a347f03e91e13eeac27847530e8bdf9dcfdb
BWangui <brigidtwangui@outlook.com>
2025-02-27 15:13:27 +0300

    Edit 1.0

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,17 @@
-import { Link } from 'react-router-dom';
-import useRecipeStore from './recipeStore';
+  // RecipeList component
+  import { useRecipeStore } from './recipeStore';
 
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
 
-  return (
-    <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default RecipeList;
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
