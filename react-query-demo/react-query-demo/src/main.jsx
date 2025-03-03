5b2d6ca4772f818143509600fbf086a55f632806
BWangui <brigidtwangui@outlook.com>
2025-03-03 19:09:21 +0300

    New

--- react-query-demo/react-query-demo/src/main.jsx
+++ react-query-demo/react-query-demo/src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from './App.jsx'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
-
 );
