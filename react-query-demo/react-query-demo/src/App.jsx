c29ca2459567d080a9bbbed89b29721a99ab07f0
BWangui <brigidtwangui@outlook.com>
2025-03-03 18:50:49 +0300

    edit all

---
+++ react-query-demo/react-query-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostsComponent from "./component/PostsComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>React Query: Fetching Posts</h1>
+    </div>
+  );
+}
+
+export default App;
