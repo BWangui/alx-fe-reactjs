00f74bfed8112fcca2dc47b4eada82078fd96998
BWangui <brigidtwangui@outlook.com>
2025-03-03 18:03:37 +0300

    installed formikform.js,refctored registrationform,

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,57 @@
+import { usestate } from "react"
+const RegistrationForm =() => {
+    const [formData, setFormData] = usestate({
+        username:"",
+        email:"",
+        password:"",
+
+    });
+
+  const [errors,setErrors] = useState({});
+  
+    const handlechange = (e) => {
+        const { name, value } =e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let validationErrors = {};
+        if (!formData.username) validationErrors.username = "Username is required";
+        if (!formData.email) validationErrors ="Email is required";
+        if (!formData.password) validationErrors.password ="Password is required";
+
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+        }else{
+            console.log("Form submitted succesfully!", formData);
+            setErrors({});
+        }
+    }
+  
+ return(
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>Username:</label>
+            <input type="text" name="username" value={formData.username} onChange={handleChange}
+            />
+            {errors.username && <p style={{color: "blue"}}>{errors.username}</p>}
+        </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" name="email" value={formData.email} onChange={handleChange}/>
+            {errors.email && <p style={{color: "blue"}}>{errors.email}</p>}
+         </div>
+             <div>
+                <label>Passwords:</label>
+                <input type="password" name="password" value={formData.password} onChange={handleChange}/>
+                {errors.password && <p style= {{ color: "blue"}}>{errors.password}</p>}
+             </div>
+        <button type="submit">Register</button>
+    </form>
+ );
+
+};
+
+export default RegistrationForm;
