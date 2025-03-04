ce54d14c619e7936a95a28eb365e6632e243c565
BWangui <brigidtwangui@outlook.com>
2025-03-04 14:46:11 +0300

    edited file

--- react-todo/src/_tests_/TodoList.test.jsx
+++ react-todo/src/_tests_/TodoList.test.jsx
@@ -3,19 +3,18 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import TodoList from "../components/TodoList";
 
-test("adds a new todo", () => {
+test("toggles todo completion", () => {
   render(<TodoList />);
 
-  
-  const inputField = screen.getByPlaceholderText("Add a new todo...");
-  const addButton = screen.getByText("Add");
 
-  
-  fireEvent.change(inputField, { target: { value: "New Todo" } });
+  const todoItem = screen.getByText("Learn React");
 
+  expect(todoItem).toHaveStyle("text-decoration: none");
 
-  fireEvent.click(addButton);
+  fireEvent.click(todoItem);
 
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+  fireEvent.click(todoItem);
 
-  expect(screen.getByText("New Todo")).toBeInTheDocument();
+  expect(todoItem).toHaveStyle("text-decoration: none");
 });
