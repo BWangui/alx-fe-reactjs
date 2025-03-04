df8574bf2da261f1110b908854d1b796feeb868d
BWangui <brigidtwangui@outlook.com>
2025-03-04 13:31:48 +0300

    modified the file

--- react-todo/src/_tests_/TodoLists.test.jsx
+++ react-todo/src/_tests_/TodoLists.test.jsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import TodoList from "../components/TodoList"; 
 
-test("renders TodoList with initial todos", () => {
-  render(<TodoList />);
+test("adds a new todo", () => {
+    render(<TodoList />);
 
-  
+    const input = screen.getByPlaceholderText(/add a new task/i);
+  const addButton = screen.getByText(/add/i);
+
+  fireEvent.change(input, { target: { value: "New Todo Item" } });
+
+ 
   expect(screen.getByText(/todo list/i)).toBeInTheDocument();
 
   expect(screen.getByText(/Learn React/i)).toBeInTheDocument();
