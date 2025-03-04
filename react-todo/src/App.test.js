2476bf500224c8155fcb4ea55fe6eb38ed82ae29
BWangui <brigidtwangui@outlook.com>
2025-03-04 17:29:01 +0300

    Changes on App.test

--- react-todo/src/App.test.js
+++ react-todo/src/App.test.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders TodoList component with initial todos', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Check that one of the initial todos is rendered.
+  const todoElement = screen.getByText(/Learn React/i);
+  expect(todoElement).toBeInTheDocument();
 });
