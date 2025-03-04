41ae8a6f4db4f8445fcb6aad2651e98bd23a862e
BWangui <brigidtwangui@outlook.com>
2025-03-04 11:37:04 +0300

    edited app.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path="settings" element={<ProfileSettings />} />
           <Route path="ProtectedRoute" element={<ProtectedRoute />} />
         </Route>
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
