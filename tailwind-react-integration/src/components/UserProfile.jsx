d44be02f70178501f61fe4368c7db8461e92b19b
BWangui <brigidtwangui@outlook.com>
2025-03-13 13:03:22 +0300

    added hover color

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration=300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">
